6356d687683b35c18b38d0deda01988b424bc66f
Allan Garcia <allang@u.northwestern.edu>
2022-09-12 14:01:36 -0500

    new maze function

--- maze_solver.py
+++ maze_solver.py
@@ -1,25 +1,30 @@
+import numpy as np
 from custom_maze import maze
 from state import find_neighbors
 
 #import maze array
 init_maze=maze
-nbors=find_neighbors(init_maze,robo_start)
 
-for i in range(len(init_maze)):
-    for j in range(len(init_maze[0])):
-        if init_maze[i][j] == 1:
-            init_maze[i][j] == 1000
-        elif init_maze[i][j]==2
-            init_maze[i][j]== 2000
-        elif init_maze [i][j]== 3
-            init_maze[i][j] ==3000
-            
 
-fin_path=path_finder(init_maze)
+
+def maze_structure(init_maze):
+    init_maze=np.reshape(init_maze,(len(init_maze),len(init_maze[0])))
+    for i in range(len(init_maze)):
+        for j in range(len(init_maze[0])):
+            if init_maze[i][j] == 1:
+                init_maze[i][j] = 1000
+            elif init_maze[i][j]==2:
+                init_maze[i][j]= 2000
+            elif init_maze [i][j]== 3:
+                init_maze[i][j] =3000
+    
+    return init_maze
+
 
 
 def path_finder(init_maze):
     maze_start=list(zip(*np.where(init_maze==3000)))
+    print(maze_start)
     robo_position=maze_start[0]
     
     for i in range(len(init_maze)):
@@ -32,6 +37,7 @@ def path_finder(init_maze):
                 elif init_maze[x][y] != init_maze[i][j]+1:
                     init_maze[x][y]=init_maze[i][j]+1
 
+    return init_maze
 
 
 
