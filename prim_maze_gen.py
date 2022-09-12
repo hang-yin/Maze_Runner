b5ae94a6cff049978ed1abcc2cb2bb9b70cf8b2c
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2022-09-12 13:02:13 -0500

    Starts generating a maze but gets stuck in the middle. Working on fixing the loop so it iterates properly.

--- prim_maze_gen.py
+++ prim_maze_gen.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-from state import find_all_neighbors
+from state import find_all_neighbors, find_wall_neighbors
 
 n = random.randint(4, 10)   # rows
 m = random.randint(4, 10)   # cols
@@ -22,27 +22,28 @@ def display_maze():
 display_maze()
 
 
-# coordinates for building free cells
+# coordinates to start building free cells
 x = random.randint(0, n-1)
 y = random.randint(0, m-1)
-
+cell_A = (x, y)
+# maze[x][y] = 0
 walls = []
 
-# maze[x][y] = 0  # random cell to start creating free cells with (A)
-neighbors = find_all_neighbors(maze, (x,y))     # returns a list of neighbor coords
-for nbr in neighbors:
-    walls.append(nbr)
 
-wall_len = len(walls)
+
+# maze[x][y] = 0  # random cell to start creating free cells with (A)
+neighbors = find_all_neighbors(maze, (x,y))     # returns a list of neighbor coords for A
+for nbr in neighbors:
+    walls.append(nbr)   # add all neighbors to wall list
+
 while len(walls) != 0:
-    cell_C = walls[random.randint(0, wall_len-1)]   # C is a wall
-    cell_A = (x, y)
+    cell_C = walls[random.randint(0, len(walls)-1)]   # C is a wall
     maze[cell_A[0]][cell_A[1]] = 0
 
     # check if C is an edge cell
     if cell_C[0] == 0 or cell_C[0] == n-1 or cell_C[1] == 0 or cell_C[1] == m-1:
         continue
-
+    
     # A and C are in the same row
     if cell_A[0] == cell_C[0]:
         # C to the right of A
@@ -71,10 +72,15 @@ while len(walls) != 0:
         else:
             cell_D = cell_B
 
+        # Make C and D free
         maze[cell_C[0]][cell_C[1]] = 0
         maze[cell_D[0]][cell_D[1]] = 0
 
-    walls.remove(cell_C)
+        neighbors_D = find_wall_neighbors(maze, cell_D)     # returns a list of neighbor coords for D
+        for nbr in neighbors_D:
+            walls.append(nbr)   # add all wall neighbors of D to wall list
+
+    walls.remove(cell_C)    # remove C from wall list
     display_maze()
 
 display_maze()
