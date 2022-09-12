f4321777a200f0a8b0122c7d7609d42d6c5e72b9
hang-yin <yinhang0226@gmail.com>
2022-09-12 11:04:09 -0500

    Added user input functions

---
+++ main.py
@@ -0,0 +1,11 @@
+from custom_maze import maze
+from state import find_neighbors
+import sys
+
+# get maze dimensions
+n = len(maze)
+m = len(maze[0])
+
+# display maze
+
+# ask for user input on start and goal position
