93da80a51a47e1919e2f44293fd266267dda3732
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2022-09-12 11:51:49 -0500

    added find_wall_neighbors function

--- state.py
+++ state.py
@@ -19,3 +19,14 @@ def find_all_neighbors(maze, position):
         if x1>=0 and x1<n and y1>=0 and y1<m:
             neighbors.append((x1,y1))
     return neighbors
+
+
+def find_wall_neighbors(maze, position):
+    n = len(maze)
+    m = len(maze[0])
+    x0,y0 = position
+    neighbors = []
+    for x1,y1 in [(x0-1,y0),(x0+1,y0),(x0,y0-1),(x0,y0+1)]:
+        if x1>=0 and x1<n and y1>=0 and y1<m and maze[x1][y1]==1:
+            neighbors.append((x1,y1))
+    return neighbors
